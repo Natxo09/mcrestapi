0612730d23929104d50e78f9c21208d0b788a189
Natxo <natxetepala@gmail.com>
2026-03-07 00:18:56 +0100

    style: add Phosphor icons, favicon and dashboard polish

--- src/main/resources/assets/mcrestapi/admin/app.js
+++ src/main/resources/assets/mcrestapi/admin/app.js
@@ -143,8 +143,8 @@
           '<td>' + perms + '</td>' +
           '<td class="text-muted">' + date + '</td>' +
           '<td style="text-align:right">' +
-            '<button class="btn btn-ghost btn-sm" onclick="editKey(\'' + escAttr(k.id) + '\')">Edit</button> ' +
-            '<button class="btn btn-danger btn-sm" onclick="deleteKey(\'' + escAttr(k.id) + '\', \'' + escAttr(k.name) + '\')">Revoke</button>' +
+            '<button class="btn btn-ghost btn-sm" onclick="editKey(\'' + escAttr(k.id) + '\')"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" viewBox="0 0 256 256"><path d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM51.31,160,136,75.31,152.69,92,68,176.68ZM48,179.31,76.69,208H48Zm48,25.38L79.31,188,164,103.31,180.69,120Zm96-96L147.31,64l24-24L216,84.68Z"></path></svg> Edit</button> ' +
+            '<button class="btn btn-danger btn-sm" onclick="deleteKey(\'' + escAttr(k.id) + '\', \'' + escAttr(k.name) + '\')"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" viewBox="0 0 256 256"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg> Revoke</button>' +
           '</td>' +
         '</tr>';
       }).join('');
@@ -304,7 +304,7 @@
         list.innerHTML = origins.map(function (o) {
           return '<div class="origin-item">' +
             '<span>' + escHtml(o) + '</span>' +
-            '<button class="btn btn-danger btn-sm" onclick="removeOrigin(\'' + escAttr(o) + '\')">Remove</button>' +
+            '<button class="btn btn-danger btn-sm" onclick="removeOrigin(\'' + escAttr(o) + '\')"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" viewBox="0 0 256 256"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg> Remove</button>' +
           '</div>';
         }).join('');
       }
